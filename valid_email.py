af5adb2db5e9c79799f7054b6ec740b1689c85c1
Neeraja0613 <147791902+Neeraja0613@users.noreply.github.com>
2025-01-28 18:46:28 +0530

    n=int(input())

---
+++ valid_email.py
@@ -0,0 +1,9 @@
+import re
+
+email = input("Enter an email: ")
+pattern = r'^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$'
+
+if re.match(pattern, email):
+    print("Valid email address")
+else:
+    print("Invalid email address")

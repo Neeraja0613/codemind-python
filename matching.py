e2802f782279cad2b9bbef6e05108962221e7c14
Neeraja0613 <147791902+Neeraja0613@users.noreply.github.com>
2025-02-02 14:18:19 +0530

    n=int(input())

---
+++ matching.py
@@ -0,0 +1,12 @@
+def http_status(status_code):
+    match status_code:
+        case 200:
+            return "OK"
+        case 404:
+            return "Not Found"
+        case 500:
+            return "Internal Server Error"
+        case _:
+            return "Unknown Status"
+
+print(http_status(200))  # Output: OK
